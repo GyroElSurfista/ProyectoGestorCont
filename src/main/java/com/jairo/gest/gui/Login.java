5670d0056335dcfe88145a62fb38684dd6b12ccc
Jairo Andree Maida Rojas <jairotrabaja123@gmail.com>
2023-01-17 09:27:13 -0400

    fix: Cambio de la ruta de los iconos

--- src/main/java/com/jairo/gest/gui/Login.java
+++ src/main/java/com/jairo/gest/gui/Login.java
@@ -184,7 +184,7 @@ public class Login extends javax.swing.JFrame {
         ojoPanel.setBackground(new java.awt.Color(255, 255, 255));
 
         ojo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        ojo.setIcon(new javax.swing.ImageIcon("C:\\Users\\Jairo\\Documents\\NetBeansProjects\\GestorContrasenias\\src\\main\\java\\com\\jairo\\gest\\gui\\assets\\ojito.png")); // NOI18N
+        ojo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ojito.png"))); // NOI18N
         ojo.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         ojo.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mousePressed(java.awt.event.MouseEvent evt) {
